80c44cc75956a654807035bef9e43c4b20d46067
zhao <zhao@yicksolutions.com>
2022-01-25 16:26:00 +0800

    feat: 更新

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
     plugins: [
         vue(),
         AutoImport({
+            imports: ['vue'],
             resolvers: [ElementPlusResolver()],
         }),
         Components({
