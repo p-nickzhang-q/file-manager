1a3af5124245ace9a0aca0febc96c83385ac5e99
zhao <zhao@yicksolutions.com>
2022-11-17 14:48:56 +0800

    feat: ✨ 图片预览可以预览当前文件夹下所有图片

--- src/util/common.ts
+++ src/util/common.ts
@@ -60,55 +60,6 @@ export type MouseOptionParam = {
     getData: GetData,
 }
 
-
-export function groupBy(arr: any[], groupPropertyNames: string[]) {
-    const map = new Map()
-    arr.forEach(item => {
-        const key = groupPropertyNames.reduce((pv, cv, index) => {
-            const cvArr = cv.split('.')
-            let value = item
-            cvArr.forEach(k => {
-                value = value[k]
-            })
-            if (index === 0) {
-                return value
-            } else {
-                return `${pv}.${value}`
-            }
-        }, '')
-        if (!map.has(key)) {
-            map.set(key, [])
-        }
-        map.get(key).push(item)
-    })
-    return Object.fromEntries(map)
-}
-
-export function groupSum(arr: any[], groupPropertyNames: string[], sumPropertyNames: string[]) {
-    const map = groupBy(arr, groupPropertyNames);
-    const sumMap: any = {}
-    Object.keys(map).forEach(key => {
-        const groupArr = map[key]
-        sumMap[key] = {}
-        sumPropertyNames.forEach(sumPropertyName => {
-            // $count, 计算分组里的个数
-            if (sumPropertyName === '$count') {
-                sumMap[key]['$count'] = groupArr.length
-            } else {
-                sumMap[key][sumPropertyName] = sum(groupArr, sumPropertyName)
-            }
-        })
-    })
-
-    return sumMap
-}
-
-export function sum(arr: any[], sumPropertyName: string) {
-    return arr.reduce((pv, cv, index) => {
-        return pv + (cv[sumPropertyName] ? cv[sumPropertyName] : 0)
-    }, 0)
-}
-
 export function longTimeFormat(time: number) {
     if (!time) {
         return ''
